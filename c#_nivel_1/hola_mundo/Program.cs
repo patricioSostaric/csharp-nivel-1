e981b671a0315b3f5b7ca2e0eba9e02aeebe8d09
patricioSostaric <patricio.jp.01sostaric@gmail.com>
2023-04-23 17:22:38 -0300

    suma de dos numeros

--- c#_nivel_1/hola_mundo/Program.cs
+++ c#_nivel_1/hola_mundo/Program.cs
@@ -4,5 +4,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("hola mundo");
+        //primer comentario 
+        
     }
 }
---
+++ c#_nivel_1/suma/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace suma;
+class Program
+{
+    static void Main(string[] args)
+    {
+         int n1;
+            int n2,resultado;
+
+
+            Console.WriteLine("ingrese un numero");
+            n1=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese otro numero");
+            n2 =int.Parse(Console.ReadLine());
+            
+            resultado=n1+n2;
+            Console.Write( "la suma es :" + resultado);
+    }
+}
